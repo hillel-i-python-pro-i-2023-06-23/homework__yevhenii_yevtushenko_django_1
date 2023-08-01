3d46a0378a72967b5bd47f787e28cfbd3ac25075
jonikkk <eugene.evtyshenko@gmail.com>
2023-08-01 13:08:21 +0200

    🎉 Begin a project.

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -5,5 +5,4 @@ app_name = "Contacts"
 
 urlpatterns = [
     path("list/", views.ContactView.as_view(), name="contacts_list"),
-    path("delete/", views.ContactDeleteView.as_view(), name="contact_delete"),
 ]
